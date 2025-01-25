6a683f896fc0f2e003390e875b2e00b390be2a5d
Anukalp Mishra <anukalp.info@gmail.com>
2025-01-25 19:47:03 +0530

    Footer section update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import "./Footer.css"
+import React, { useEffect } from 'react';
+import "./Footer.css";
 
 function Footer() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://counter6.optistats.ovh/private/counter.js?c=l3tpwwhrdhbw31xfxa7rnhnhhbhdze62&down=async";
+    script.async = true;
+    script.onload = () => console.log("Visitor counter script loaded successfully.");
+    script.onerror = () => console.error("Error loading visitor counter script.");
+    document.getElementById('sfcl3tpwwhrdhbw31xfxa7rnhnhhbhdze62').appendChild(script);
+  }, []);
+
   return (
     <>
-    <div className='footer'>
+      <div className='footer'>
         <div className='author'>
-        <p><i class="fa-sharp fa-solid fa-code fa-beat"></i>&nbsp; with <i className="fa-solid fa-heart fa-bounce" style={{color: "red"}}></i> by<a href="https://github.com/anukalp-mishra/MyResume/blob/main/Anukalp%20Mishra%20Resume.pdf" target="_blank" rel="noopener noreferrer"> Anukalp</a></p>
+          <p>
+            <i className="fa-sharp fa-solid fa-code fa-beat"></i>&nbsp; with <i className="fa-solid fa-heart fa-bounce" style={{color: "red"}}></i> by
+            <a href="https://github.com/anukalp-mishra/MyResume/blob/main/Anukalp%20Mishra%20Resume.pdf" target="_blank" rel="noopener noreferrer"> Anukalp</a>
+          </p>
         </div>
         <div className=''>
-        <p className='copyright'> <i className='far fa-copyright'></i> {new Date().getFullYear()} All rights reserved.</p>
+          <p className='copyright'>
+            <i className='far fa-copyright'></i> {new Date().getFullYear()} All rights reserved.
+          </p>
         </div>
-    </div>
+        {/* Visitor Counter */}
+        <div id="sfcl3tpwwhrdhbw31xfxa7rnhnhhbhdze62"></div>
+        <noscript>
+          <a href="https://www.freecounterstat.com" title="free counter">
+            <img src="https://counter6.optistats.ovh/private/freecounterstat.php?c=l3tpwwhrdhbw31xfxa7rnhnhhbhdze62" border="0" title="free counter" alt="free counter" />
+          </a>
+        </noscript>
+      </div>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
