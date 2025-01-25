f4fcf0068d8efae03c8f39577d0e0ac56149c0b2
Anukalp Mishra <anukalp.info@gmail.com>
2025-01-25 19:30:00 +0530

    Visitors count update

--- src/App.js
+++ src/App.js
@@ -10,25 +10,32 @@ import Project from './views/Project/Project';
 import Contact from "./views/Contact/Contact"
 import Footer from './components/Footer/Footer'
 
-
-
 function App() {
   return (
     <>     
-    <div className='body'>      
-      <div id="loading_start" className='loading_start_none'>
-        <p>Loading</p>
-        <PulseLoader color={"#05e6ee"} size={12} />
-      </div>
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Service/>
-      <Project/>
-      <Contact/>
-      <Footer/>
-    </div> 
+      <div className='body'>      
+        <div id="loading_start" className='loading_start_none'>
+          <p>Loading</p>
+          <PulseLoader color={"#05e6ee"} size={12} />
+        </div>
+        <Navbar/>
+        <Home/>
+        <About/>
+        <Service/>
+        <Project/>
+        <Contact/>
+        <Footer/>
+        {/* Visitor Counter */}
+        <div id="sfcl3tpwwhrdhbw31xfxa7rnhnhhbhdze62"></div>
+        <script type="text/javascript" src="https://counter6.optistats.ovh/private/counter.js?c=l3tpwwhrdhbw31xfxa7rnhnhhbhdze62&down=async" async></script>
+        <noscript>
+          <a href="https://www.freecounterstat.com" title="free counter">
+            <img src="https://counter6.optistats.ovh/private/freecounterstat.php?c=l3tpwwhrdhbw31xfxa7rnhnhhbhdze62" border="0" title="free counter" alt="free counter" />
+          </a>
+        </noscript>
+      </div> 
     </>
   );
 }
+
 export default App;
